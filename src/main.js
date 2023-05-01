a487e8deb6f327675b5d18ab62f4773f947da97c
KCNS2027 <131724433+KCNS2027@users.noreply.github.com>
2023-04-30 21:46:29 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
+
 import { Application } from '@splinetool/runtime';
 
-function loadScene() {
-  const canvas = document.getElementById('PrisonMap') || document.getElementById('canvas3d') || document.getElementById('Memorial');
-  const app = new Application(canvas);
-
-  if (canvas.id === 'PrisonMap') {
-    app.load('https://prod.spline.design/vofWFSaFayA4dGmw/scene.splinecode');
-  } else if (canvas.id === 'Memorial') {
-    app.load('https://prod.spline.design/jpU6DCkEjKpVyGvF/scene.splinecode');
-  } else {
-    app.load('https://prod.spline.design/2YRVkX8xQB16LEWt/scene.splinecode');
-  }
-}
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/2YRVkX8xQB16LEWt/scene.splinecode');
 
