f3f951593f01ef11655e0ae1aef72edd0b79d3e9
KCNS2027 <131724433+KCNS2027@users.noreply.github.com>
2023-04-30 21:47:55 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/2YRVkX8xQB16LEWt/scene.splinecode');
+app.load('https://prod.spline.design/vofWFSaFayA4dGmw/scene.splinecode');
 
